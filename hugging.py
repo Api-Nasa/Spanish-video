c5f2ac5d987c4659cb6e69aef0a81a0ce0ea4ef0
IBM-SkillBuild <Api.Nasa.2022@gmail.com>
2024-10-26 07:39:33 +0200

    requirements new

--- hugging.py
+++
@@ -1,9 +0,0 @@
-import dl_translate as dlt
-mt = dlt.TranslationModel(device="auto") # ("cpu" or "gpu")  Slow when you load it for the first time 
-#print(mt.available_languages())  # All languages that you can use
-#print(mt.available_codes())  # Code corresponding to each language accepted
-#print(mt.get_lang_code_map())  # Dictionary of lang -> code
-
-for i in range(5):
-    text_hi = "i am doing things today, but i am worry.  ?"
-    print(mt.translate(text_hi,  source="English", target="es"))
